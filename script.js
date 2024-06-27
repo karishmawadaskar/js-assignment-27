61262c6421cd6bcf3c3943740c83942bf2bb301b
karishmawadaskar <karishmawadaskar@gmail.com>
2024-06-26 17:55:26 -0800

    addedscriptpage

---
+++ script.js
@@ -0,0 +1,33 @@
+function mouseevent1() {
+    console.log(`mouse over image`)
+}
+function mouseevent2() {
+    console.log(`mouse move on image`)
+}
+function mouseevent3() {
+    console.log(`mouse out from image`)
+}
+
+function keysevent1() {
+    console.log(`***key press***`)
+}
+function keysevent2() {
+    console.log(`___key down___`)
+}
+function keysevent3() {
+    console.log(`----key up----`)
+}
+
+function clickevent1() {
+    console.log(`**event of onclick**`)
+}
+function clickevent2() {
+    console.log(`##event of ondblclick##`)
+}
+
+function changeevent(){
+    console.log(`text has changed.....`)
+}
+function loadevent(){
+   alert(`page reload...`)
+}
